86ebe5d94f49696bf58a41d9ef19d189a4a8bed6
Ivan Yaremko <yaremich98@gmail.com>
2023-03-17 08:46:32 +0200

    (64) React. PropTypes.date-fns

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
-import BlockCounter from './components/BlockCounter';
+import React, {Component} from 'react';
+import Ciao from './components/CiaoSection/Ciao';
+import UsersList from './components/UsersList';
+import UserCard from './components/UserCard';
+import Calendar from './components/Calendar';
 
 
 function App() {
-    return (
-      <BlockCounter />
-    );
+  const user = {
+    id:1,
+    name:'Elon'
   }
-  
+  const user2 = {
+    id:1,
+    name:'Elen'
+  }
+  const user3 = {
+    id:2,
+    name:'Elan'
+  }
+    return <>
+      <Ciao name={user.name} id={user.id} />
+      <UserCard user={user2} />
+      <UsersList users={[user2, user3]} />
+      <Calendar />
+    </>
+}
+
   export default App;
